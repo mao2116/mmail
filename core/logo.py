9ef46de879d1150abf1ead603d13e3e7fed7564c
MAO-COMMUNITY <78520987+mao2116@users.noreply.github.com>
2021-11-16 08:55:54 +0600

    Add files via upload

---
+++ core/logo.py
@@ -0,0 +1,45 @@
+acl='\033[1;30m'
+rcl='\033[1;31m'
+gcl='\033[1;32m' 
+ycl='\033[1;33m'
+bcl = '\033[1;34m'
+pcl = '\033[1;35m'
+ccl='\033[1;36m' 
+wcl='\033[1;37m'
+mcl = '\033[1;94m'
+ncl='\033[0;00m'
+rcl='\033[1;31m'
+gcl='\033[1;32m' 
+ycl='\033[1;33m'
+bcl = '\033[1;34m'
+pcl = '\033[1;35m'
+ccl='\033[1;36m' 
+wcl='\033[1;37m'
+mcl = '\033[1;94m'
+ncl='\033[0;00m'
+loginpromt="""
+{acl} __  __  {ycl} __  __    _    ___ _
+{acl}|  \/  | {ycl}|  \/  |  / \  |_ _| |
+{acl}| |\/| | {ycl}| |\/| | / _ \  | || |
+{acl}| |  | | {ycl}| |  | |/ ___ \ | || |___
+{acl}|_|  |_| {ycl}|_|  |_/_/   \_\___|_____|{ncl}
+        
+        {acl}[ {bcl}CODER {acl}] {pcl}MAO2116
+        {acl}[ {bcl}GITHUB {acl}]{pcl} MAO2116    
+        {acl}[ {bcl}THANKS TO {acl}]{pcl} ALLAH
+        
+        {acl}[{gcl} !{acl} ] {rcl}ANALYSING  DATA{acl}[{gcl} !{acl} ]{ncl}
+""".format(acl=acl,ncl=ncl,pcl=pcl,ycl=ycl,bcl=bcl,gcl=gcl,rcl=rcl)
+manuhome="""
+{acl} __  __  {ycl} __  __    _    ___ _
+{acl}|  \/  | {ycl}|  \/  |  / \  |_ _| |
+{acl}| |\/| | {ycl}| |\/| | / _ \  | || |
+{acl}| |  | | {ycl}| |  | |/ ___ \ | || |___
+{acl}|_|  |_| {ycl}|_|  |_/_/   \_\___|_____|{ncl}
+        
+        {acl}[ {bcl}CODER {acl}] {pcl}MAO2116
+        {acl}[ {bcl}GITHUB {acl}]{pcl} MAO2116    
+        {acl}[ {bcl}THANKS TO {acl}]{pcl} ALLAH
+        
+        {acl}[{gcl} !{acl} ] {rcl}MANU {acl}[{gcl} !{acl} ]{ncl}
+""".format(acl=acl,ncl=ncl,pcl=pcl,ycl=ycl,bcl=bcl,gcl=gcl,rcl=rcl)
