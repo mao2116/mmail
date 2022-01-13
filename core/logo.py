efb481836e5c8aa2afe43855ff8247f326ca6320
MAO-COMMUNITY <78520987+mao2116@users.noreply.github.com>
2022-01-13 18:48:09 +0600

    Update logo.py

--- core/logo.py
+++ core/logo.py
@@ -18,7 +18,7 @@ wcl='\033[1;37m'
 mcl = '\033[1;94m'
 ncl='\033[0;00m'
 loginpromt="""
-{acl} __  __  {ycl} __  __    _    ___ _
+{acl} __  _V1.1{ycl}__  __    _    ___ _
 {acl}|  \/  | {ycl}|  \/  |  / \  |_ _| |
 {acl}| |\/| | {ycl}| |\/| | / _ \  | || |
 {acl}| |  | | {ycl}| |  | |/ ___ \ | || |___
@@ -26,12 +26,12 @@ loginpromt="""
         
         {acl}[ {bcl}CODER {acl}] {pcl}MAO2116
         {acl}[ {bcl}GITHUB {acl}]{pcl} MAO2116    
-        {acl}[ {bcl}THANKS TO {acl}]{pcl} ALLAH
+        {acl}[ {bcl}THANKS TO {acl}]{pcl} ALLAH !
         
         {acl}[{gcl} !{acl} ] {rcl}ANALYSING  DATA{acl}[{gcl} !{acl} ]{ncl}
 """.format(acl=acl,ncl=ncl,pcl=pcl,ycl=ycl,bcl=bcl,gcl=gcl,rcl=rcl)
 manuhome="""
-{acl} __  __  {ycl} __  __    _    ___ _
+{acl} __  __V1.1{ycl}_  __    _    ___ _
 {acl}|  \/  | {ycl}|  \/  |  / \  |_ _| |
 {acl}| |\/| | {ycl}| |\/| | / _ \  | || |
 {acl}| |  | | {ycl}| |  | |/ ___ \ | || |___
@@ -39,7 +39,26 @@ manuhome="""
         
         {acl}[ {bcl}CODER {acl}] {pcl}MAO2116
         {acl}[ {bcl}GITHUB {acl}]{pcl} MAO2116    
-        {acl}[ {bcl}THANKS TO {acl}]{pcl} ALLAH
+        {acl}[ {bcl}THANKS TO {acl}]{pcl} ALLAH !
         
         {acl}[{gcl} !{acl} ] {rcl}MANU {acl}[{gcl} !{acl} ]{ncl}
 """.format(acl=acl,ncl=ncl,pcl=pcl,ycl=ycl,bcl=bcl,gcl=gcl,rcl=rcl)
+def inboxlogo(currentemail):
+  inboxlogo_="""
+{acl} __  _V1.1{ycl}__  __    _    ___ _
+{acl}|  \/  | {ycl}|  \/  |  / \  |_ _| |
+{acl}| |\/| | {ycl}| |\/| | / _ \  | || |
+{acl}| |  | | {ycl}| |  | |/ ___ \ | || |___
+{acl}|_|  |_| {ycl}|_|  |_/_/   \_\___|_____|{ncl}
+        
+        {acl}[ {bcl}CODER {acl}] {pcl}MAO2116
+        {acl}[ {bcl}GITHUB {acl}]{pcl} MAO2116    
+        {acl}[ {bcl}THANKS TO {acl}]{pcl} ALLAH !
+        
+{acl}[{bcl} PRESS{gcl} CTRL+C {acl}]{ycl} TO OPEN MAIN MANU   
+
+{acl}[ {gcl}CURRENT{acl}_{mcl}MAIL {acl}] {ycl}{currentemail}
+
+        {acl}[{gcl} !{acl} ] {rcl}INBOX {acl}[{gcl} !{acl} ]{ncl}
+""".format(mcl=mcl,acl=acl,ncl=ncl,pcl=pcl,ycl=ycl,bcl=bcl,gcl=gcl,rcl=rcl,currentemail=currentemail)
+  print(inboxlogo_)
